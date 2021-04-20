40f9174d76fcb74b9f17a52675bf6b90ae62ba47
MGoBlue07 <landonharter@outlook.com>
2021-04-20 18:32:22 -0400

    Moved server to unity for collisions and physics

---
+++ GameServer(Unity)/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+
+    public static NetworkManager Instance;
+
+    public GameObject playerPrefab;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(Instance);
+    }
+
+    public Player InstantiatePlayer()
+    {
+        return Instantiate(playerPrefab, Vector3.zero, Quaternion.identity).GetComponent<Player>();
+    }
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+        #if UNITY_EDITOR
+        Debug.Log("Build the server before playing.");
+        #else
+        Server.Start(10, 444);
+        #endif
+    }
+}
